da3e1130738972c7390be42c215e5f8c48803804
唐立博 <2889668653@qq.com>
2020-02-11 16:37:56 +0800

    Initial commit

---
+++ javainner/src/com/atguigu/Customer.java
@@ -0,0 +1,9 @@
+package com.atguigu;
+
+/**
+ * @author TangLibo
+ * @create 2020-02-11 15:27
+ */
+public class Customer {
+
+}
---
+++ src/com/atguigu/bean/Hello.java
@@ -0,0 +1,11 @@
+package com.atguigu.bean;
+
+import com.atguigu.Customer;
+
+/**
+ * @author TangLibo
+ * @create 2020-02-11 15:27
+ */
+public class Hello {
+    Customer c ;
+}
